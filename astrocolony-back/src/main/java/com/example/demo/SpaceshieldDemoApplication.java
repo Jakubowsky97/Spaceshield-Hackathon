a284d04cb0735933dcee918ba93d24752c23625d
Jakub Mądry <jacobn0x@MacBook-Air-Jakub.local>
2025-05-24 11:38:55 +0200

    feat: initialize astrocolony-front with React, Vite, and Tailwind CSS
    - Added package.json for project dependencies and scripts
    - Created postcss.config.js for Tailwind CSS and autoprefixer
    - Implemented main application structure in App.tsx and main.tsx
    - Configured Tailwind CSS in tailwind.config.ts
    - Set up TypeScript configurations in tsconfig.app.json, tsconfig.node.json, and tsconfig.json
    - Added Vite configuration in vite.config.ts
    - Removed outdated README files from backend and frontend directories

---
+++ astrocolony-back/src/main/java/com/example/demo/SpaceshieldDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceshieldDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceshieldDemoApplication.class, args);
+	}
+
+}
